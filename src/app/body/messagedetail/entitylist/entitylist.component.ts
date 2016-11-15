3340511412d16b1ee7f24fcf8bf014cb55052d7a
Adam Wang <hwang12wou@gmail.com>
2016-11-15 17:20:42 -0500

    aha

--- src/app/body/messagedetail/entitylist/entitylist.component.ts
+++ src/app/body/messagedetail/entitylist/entitylist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { Entitylist } from './entitylist';
 import { EntitylistRecord } from './entitylistRecord';
 
@@ -8,86 +8,11 @@ import { EntitylistRecord } from './entitylistRecord';
   styleUrls: ['./entitylist.component.css']
 })
 export class EntitylistComponent implements OnInit {
-  private entitylist: Entitylist;
-  private flag = true;
-  private entitylistData = [
-    {
-      "tin": "10001",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 1,
-      "address": "address info1",
-      "mainEntity": "IndustryCo"
-    },
-    {
-      "tin": "10002",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 0,
-      "address": "address info2",
-      "mainEntity": "IndustryCo"
-    },
-    {
-      "tin": "10003",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 0,
-      "address": "address info3",
-      "mainEntity": "IndustryCo"
-    },{
-      "tin": "10004",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 0,
-      "address": "address info4",
-      "mainEntity": "IndustryCo"
-    },{
-      "tin": "10005",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 1,
-      "address": "address info5",
-      "mainEntity": "IndustryCo"
-    },{
-      "tin": "10006",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 1,
-      "address": "address info6",
-      "mainEntity": "IndustryCo"
-    },
-    {
-      "tin": "10007",
-      "name": "Global...",
-      "entityCode": "Global...",
-      "docType": "CBCR",
-      "incorporationCode": "Global...",
-      "resident": "CA",
-      "isPE": 0,
-      "address": "address info7",
-      "mainEntity": "IndustryCo"
-    }
-  ];
+  @Input() messageDetail;
+  @Input() currentDoc;
+
   constructor() {
-    this.entitylist = new Entitylist(this.entitylistData);
+
   }
 
   ngOnInit() {
