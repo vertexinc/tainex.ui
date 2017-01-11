92cb6cfacd29267fcadabf0bac2399f4978ef7f7
Adam Wang <hwang12wou@gmail.com>
2017-01-11 16:15:37 -0500

    important: ngonchanges

--- src/app/body/messagedetail/message/message.component.ts
+++ src/app/body/messagedetail/message/message.component.ts
@@ -13,18 +13,24 @@ export class MessageComponent implements OnInit {
   @Input() messageDetail;
   @Input() language;
 
-  model = new Message('test');
+  model = new Message();
 
   constructor(private _tieappService: TieappService) {
 
   }
 
+  ngOnChanges() {
+    JSON.stringify(alert('this.messageDetail' + JSON.stringify(this.messageDetail)));
+    this.model.subject = this.messageDetail.subject;
+  }
   ngOnInit() {
     // alert(JSON.stringify(this.messageDetail))
-    JSON.stringify(alert('this.messageDetail' + this.messageDetail));
-    JSON.stringify(alert('this.messageDetail.subject' + this.messageDetail.subject));
-    this.model.subject = 'test2';
-    this.model.description = 'desc2';
+    // JSON.stringify(alert('this.messageDetail' + JSON.stringify(this.messageDetail)));
+    // JSON.stringify(alert('this.messageDetail.subject' + JSON.stringify(this.messageDetail.subject)));
+    let subject = this.messageDetail.subject;
+    alert(this.messageDetail.subject + subject)
+    // this.model.subject = 'test2';
+    // this.model.description = 'desc2';
   }
 
   get diagnostic() { return JSON.stringify(this.model); }
