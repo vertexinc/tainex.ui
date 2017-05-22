2c6dd00ac4337ccc7bb20045580fc988457a07e5
Adam Wang <hwang12wou@gmail.com>
2017-05-22 14:26:20 -0400

    front end only

--- src/app/tieapp.service.ts
+++ src/app/tieapp.service.ts
@@ -10,8 +10,8 @@ import { Headers, RequestOptions } from '@angular/http';
 @Injectable()
 export class TieappService {
   //private _url = "tieapp.data.json";
-  private _url = "./app/tieapp.data.json";
-  private _currentMsgUrl = "./app/tieapp.tieMainPageData.json";
+  //private _url = "./app/tieapp.data.json";
+  private _tieMainPageUrl = "./app/tieapp.tieMainPageData.json";
   private currentUrl = 'login';
   private _currentDocUrl = "./app/tieapp.tieDoc.json";
 
@@ -22,104 +22,120 @@ export class TieappService {
 
 
   getData(): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "initPage" });
-
-    return this._http.post(this.currentUrl, param, options)
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "initPage" });
+    //
+    // return this._http.post(this._tieMainPageUrl, param, options)
       //return this._http.get(this._url)
+      return this._http.get(this._tieMainPageUrl)
       .map(res => res.json());
   }
 
   sendMessage(messageId): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "send", messageId: messageId });
-
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._url)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "send", messageId: messageId });
+    //
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._url)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
 
   resetMsgandDoc(): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "reset" });
-
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._url)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "reset" });
+    //
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._url)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
 
   deleteCurrentMsg(): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "deleteMsg" });
-
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._url)
-      .map(res => res.json());
-
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "deleteMsg" });
+    //
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._url)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
 
   //Detach Doc
   postDetachedDocId(docIdList): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "detachDoc", docIdListString: docIdList });
-    return this._http.post(this.currentUrl, param, options)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "detachDoc", docIdListString: docIdList });
+    // return this._http.post(this.currentUrl, param, options)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
 
   //Attach doc
   postDoc(docString, fileName): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "saveDoc", docString: docString, fileName: fileName });
-    // alert("ready to save doc: " + param)
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._currentMsgUrl)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "saveDoc", docString: docString, fileName: fileName });
+    // // alert("ready to save doc: " + param)
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._currentMsgUrl)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
 
   }
 
   postSave(model): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "save", tieMsg: model });
-    // alert("ready to post" + param)
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._currentMsgUrl)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "save", tieMsg: model });
+    // // alert("ready to post" + param)
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._currentMsgUrl)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
 
   }
 
   postCurrentMsg(messageId): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "selectCurrentMsg", messageId: messageId });
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._currentMsgUrl)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "selectCurrentMsg", messageId: messageId });
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._currentMsgUrl)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
 
   postCurrentDoc(docId): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
-    let options = new RequestOptions({ headers: headers });
-    let param = JSON.stringify({ action: "selectCurrentDoc", docId: docId });
-    return this._http.post(this.currentUrl, param, options)
-      //return this._http.get(this._currentDocUrl)
-      .map(res => res.json());
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // // let headers = new Headers({ 'Content-Type': 'text/plain; charset=UTF-8' });
+    // let options = new RequestOptions({ headers: headers });
+    // let param = JSON.stringify({ action: "selectCurrentDoc", docId: docId });
+    // return this._http.post(this.currentUrl, param, options)
+    //   //return this._http.get(this._currentDocUrl)
+    //   .map(res => res.json());
+    return this._http.get(this._tieMainPageUrl)
+    .map(res => res.json());
   }
-  getCurrentMsg() {
-    // return this.message;
-    return this._http.get(this._currentMsgUrl)
-      .map(res => res.json());
-  }
+  // getCurrentMsg() {
+  //   // return this.message;
+  //   return this._http.get(this._currentMsgUrl)
+  //     .map(res => res.json());
+  // }
 
   getCurrentDoc() {
     // return this.message;
